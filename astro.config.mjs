d6c3d97ff5b5b8868770d9fad79c185d763cafec
tae1344 <xodus1344@gmail.com>
2026-02-24 01:16:34 +0900

    feat: Add support for mathematical expressions in markdown using remark-math and rehype-katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,13 +9,16 @@ import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import rehypeFigureFromImage from './src/plugins/rehype-figure-from-image.mjs';
 import mermaid from 'astro-mermaid';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tae1344.github.io',
   integrations: [react(), sitemap(), partytown(), mdx(), mermaid({autoTheme: true})],
   markdown: {
-    rehypePlugins: [rehypeFigureFromImage],
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex, rehypeFigureFromImage],
     syntaxHighlight: {
       type: 'shiki',
       excludeLangs: ['math'],
